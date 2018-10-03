b37c15d57be3328ee8298f0868e6d3b3135014af
Narek Ohanjanyan <narekohanjanyan55@gmail.com>
2018-10-03 13:50:15 +0400

    Updated Jest. Added new devdependencies

--- src/components/svg-marks.js
+++ src/components/svg-marks.js
@@ -1,6 +1,8 @@
-import {Component} from "react";
-import React from "react";
 import '../App.css';
+import {Component} from "react";
+
+import React from "react";
+
 
 /** Class representing an SvgMarks. */
 class SvgMarks extends Component{
